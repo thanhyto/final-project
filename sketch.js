a83ba6203b4bb98b226940c4483289d022d8866b
Thanh To <thanhyto.per@gmail.com>
2023-05-08 16:47:35 -0700

    add random color to boxes, add ground, add friction and restitution for boxes

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let Engine = Matter.Engine,
     World = Matter.World;
 
 let engine = Engine.create();
-let runner = Runner.create();
+let runner = Runner.create({wireframes:false});
 let world = engine.world;
 
 let box = [];
@@ -17,23 +17,38 @@ function setup(){
     
     rectMode(CENTER);
     Runner.run(runner,engine);
+    let options = {
+        isStatic: true,
+    }
+    ground = Bodies.rectangle(windowWidth/2, windowHeight, windowWidth, 40, options);
+    World.add(world, ground);
+    
     
 }
 
 function Boxes(x,y,w,h){
     this.w = w;
     this.h = h;
-    //Create a rectangle starts x,y - width w - height h
-    this.body = Bodies.rectangle(x,y,w,h);
 
+    let options = {
+        friction: 0.5,
+        restitution: 2
+    }
+    //Create a rectangle starts x,y - width w - height h
+    this.body = Bodies.rectangle(x,y,w,h, options);
+    
     //Add to physical world
     World.add(world,this.body);
-
+    let c = color(random(255), random(255), random(255));
     this.show = function () {
         let pos = this.body.position;
         let angle = this.body.angle;
         push();
         translate(pos.x,pos.y);
+        rotate(angle);
+        strokeWeight(1);
+        stroke(255);
+        fill(c);
         //Draw a rectangle with this.body.position.x and this.body.position.y
         rect(0,0,this.w,this.h);
         pop();
@@ -51,5 +66,10 @@ function draw(){
         //Called show() since declare as a function
         box[i].show();
     }
+
+    //Ground
+    noStroke();
+    fill(0);
+    rect(windowWidth/2, windowHeight, windowWidth, 40);
 }
 
